13719a2f873197fa242aeb3dab32b05bcb672322
Ícaro Gavazza <icarodgl@gmail.com>
2024-05-16 21:17:47 -0300

    loading

---
+++ src/components/loading/loading.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import { usersStore } from "@/stores";
+const userStore = usersStore();
+const {isLoading} = storeToRefs(userStore);
+</script>
+
+<template>
+    <div v-if="isLoading" class="loading">
+    <span  class="spinner-border spinner-border-lg align-center"></span>
+    </div>
+</template>
+
+<style>
+.loading{
+    position: absolute;
+    z-index: 99;
+    background-color: #ffffff1a;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    left: 0;
+}
+.spinner-border{
+    color: black;
+    height: 100px;
+    width: 100px;
+}
+</style>
