0a3b4be18af3a95eb7dbcf64f6a12523b34f8bd8
Ícaro Gavazza <icarodgl@gmail.com>
2024-05-20 17:05:26 -0300

    criado: campo com mascara

---
+++ src/components/forms/form-group-field.vue
@@ -0,0 +1,28 @@
+
+<script setup>
+import { Field } from 'vee-validate';
+const props = defineProps(['name','validations','errors', 'dado'])
+defineEmits(['change'])
+
+
+</script>
+
+
+<template>
+    <div class="form-group">
+        <label>{{name}}</label>
+        <Field 
+        class="form-control" 
+        type="text" 
+        :name="name" 
+        :value="dado"
+        :class="{ 'is-invalid': errors[name] }" 
+        @input="$emit('change',$event.target.value)"
+        />
+    <div class="invalid-feedback">{{ errors.nome }}</div>
+    </div>
+</template>
+
+<style>
+
+</style>
