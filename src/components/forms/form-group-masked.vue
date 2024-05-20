54fe6492300243f5fa238f6bb9dc6f22d033f5f9
Ícaro Gavazza <icarodgl@gmail.com>
2024-05-20 17:05:45 -0300

    criado campo com mascara

---
+++ src/components/forms/form-group-masked.vue
@@ -0,0 +1,34 @@
+
+<script setup>
+// https://vuejs-tips.github.io/vue-the-mask/
+
+const props = defineProps(['name','validations','errors','mascara', 'dado'])
+defineEmits(['change'])
+
+const maskConfig = {
+    telefone: ['(##) ####-####', '(##) #####-####'],
+    cpf:  '###.###.###-##',
+    data: '##/##/####'
+}
+</script>
+
+
+<template>
+    <div class="form-group">
+        <label>{{name}}</label>
+        <input 
+        class="form-control" 
+        type="text" 
+        v-mask="maskConfig[mascara]" 
+        :name="name" 
+        :value="dado"
+        :class="{ 'is-invalid': errors[name] }" 
+        @input="$emit('change',$event.target.value)"
+        />
+    <div class="invalid-feedback">{{ errors.nome }}</div>
+    </div>
+</template>
+
+<style>
+
+</style>
